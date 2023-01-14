1487dfa30b38ee0eab318b1536db612772537108
Marcelo <pbsmarcelo@gmail.com>
2023-01-14 19:30:09 -0300

    Add propriedade delay - devido a textos longos

--- cypress/integration/projeto_teste.spec.js
+++ cypress/integration/projeto_teste.spec.js
@@ -2,11 +2,19 @@
 
 describe('Projeto teste', function() {
     beforeEach(function(){
-        cy.visit('https://ultimateqa.com/automation')
+        cy.visit('http://www.aprendendotestar.com.br/treinar-automacao.php')
     })
 
     it('acessar página principal e verificar o título da url com sucesso', function() {
-        cy.title().should('be.equal', 'Automation Practice - Ultimate QA')
+        cy.title().should('be.equal', 'Sobre - Aprendendo a Testar - Um guia para você aprender sobre testes de Software')
+    })
+
+    it.only('preencher campos e enviar formulário com sucesso', function(){
+        cy.get('input[type="text"][name="form_usuario"]').type('marcelo.patrik')
+        cy.get('input[type="password"][name="form_senha"]').type('123456789')
+        cy.get('input[type="text"][name="form_nome"]').type('Marcelo Patrik Bitencourt da Silva', { delay : 0 })
+        cy.get('input[type="submit"]').click()
+        cy.contains('marcelo.patrik').should('be.visible')
     })
 
   })

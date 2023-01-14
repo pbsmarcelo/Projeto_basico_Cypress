b1f7f18c1a54bdefb047ead6913513e884dc1e13
Marcelo <pbsmarcelo@gmail.com>
2023-01-14 19:57:47 -0300

    Adicionando testes

--- cypress/integration/projeto_teste.spec.js
+++ cypress/integration/projeto_teste.spec.js
@@ -9,13 +9,21 @@ describe('Projeto teste', function() {
         cy.title().should('be.equal', 'Sobre - Aprendendo a Testar - Um guia para você aprender sobre testes de Software')
     })
 
-    it.only('preencher campos e enviar formulário com sucesso', function(){
+    it('cadastrar usuário com sucesso', function(){
         cy.get('input[type="text"][name="form_usuario"]').type('marcelo.patrik')
         cy.get('input[type="password"][name="form_senha"]').type('123456789')
         cy.get('input[type="text"][name="form_nome"]').type('Marcelo Patrik Bitencourt da Silva', { delay : 0 })
         cy.get('input[type="submit"]').click()
         cy.contains('marcelo.patrik').should('be.visible')
     })
+
+    it('exibir mensagem erro quando o usuario não preencher o campo nome', function(){
+        cy.get('input[type="text"][name="form_usuario"]').type('Edson Jose')
+        cy.get('input[type="password"][name="form_senha"]').type('123456789')
+        cy.get('input[type="submit"]').click()
+        cy.contains('Existem campos em branco.').should('be.visible')
+    })
+
 
   })
   
